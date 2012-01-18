c25decd11ba2a28a55deb7170b79e7513ca8e655
Alick Zhao <alick9188@gmail.com>
2012-01-18 16:47:14 +0800

    fixed conforming to tidy

--- tidy-all.sh
+++ tidy-all.sh
@@ -3,16 +3,24 @@
 
 newfile=".new"
 
-for file in html/*.html; do
+if [[ -d ".git" && -f "tidy.conf" ]]; then
+    :
+else
+    echo "Please run this script from the top working directory." 1>&2
+    exit 1
+fi
+
+for file in index.html html/*.html; do
     echo "Doing $file"
     newfile="${file}.new"
     tidy -q -config ./tidy.conf -o "$newfile" "$file"
     if [ $? -eq 0 ]; then
         cp "$newfile" "$file"
     else
-        echo "Error with $file" >&2
+        echo "Error with $file" 1>&2
     fi
     rm "$newfile"
     echo "Done with $file"
     #sleep 1
 done
+# vim:set et sw=4:
